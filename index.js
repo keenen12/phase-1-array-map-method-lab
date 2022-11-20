3c32d900b7a0a8a1b6d34b5fc28eaa732f24b799
Keenen Harral <kharral23@yahoo.com>
2022-11-20 10:12:22 -0800

    commit tree 54f2c76c4873246212f4669582c572d539b8488a

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+ const titleCased = () => tutorials.map(str => {
+  const arr = str.split(" ");
+  for (var i = 0; i < arr.length; i++) {
+    arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+  }
+  const str2 = arr.join(' ');
+  console.log(str2)
+  return str2;
+ })
+
